44fb60196a2833a459e28430e59866fa1a2999ae
徐康 <7836246@qq.com>
2024-09-05 11:44:16 +0800

    初始化项目

--- app.vue
+++ app.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLoadingIndicator/>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+<style>
+@media (min-width: 400px) and (prefers-reduced-motion: no-preference) {
+  .page-enter-active,
+  .page-leave-active {
+    transition: all 0.3s;
+  }
+
+  .page-enter-from,
+  .page-leave-to {
+    opacity: 0;
+    filter: blur(4px);
+    transform: translateY(20px);
+  }
+
+  .layout-enter-active,
+  .layout-leave-active {
+    transition: all 0.4s;
+  }
+
+  .layout-enter-from,
+  .layout-leave-to {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,63 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: {enabled: true},
+  // 第三方模块
+  modules: [
+    "@nuxt/devtools",
+    '@nuxtjs/google-fonts',
+  ],
+  app: {
+    pageTransition: {name: 'page', mode: 'out-in'},
+    layoutTransition: {name: 'layout', mode: 'out-in'},
+  },
+  googleFonts: {
+    families: {
+      'Space Grotesk': {
+        wght: [400, 500, 600, 700, 800, 900]
+      },
+      'Noto Sans Thai Looped': {
+        wght: [400, 600, 700, 800]
+      },
+      'Fira Code': {
+        wght: [400, 600, 700, 800]
+      }
+    }
+  },
+  //打包配置
+  nitro: {
+    //缩小捆绑包
+    minify: true,
+    //关闭源映射生成
+    sourceMap: false,
+    // 压缩公共资产
+    compressPublicAssets: true
+  },
+  // Vite 配置
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 1000,
+    },
+    // plugins: plugin,
+  },
+  //取消取消行内样式
+  features: {
+    inlineStyles: false,
+  },
+  // 构建配置
+  experimental: {
+    //  禁用 Vue3 兼容性
+    externalVue: false
+  },
+  // @ts-ignore 设置压缩
+  meta: {
+    renderSSRHeadOptions: {
+      // typeof RenderSSRHeadOptions @unhead/schema
+      omitLineBreaks: true,
+    },
+  },
+  // 构建配置 启用 Nuxt4 兼容性
+  future: {
+    compatibilityVersion: 4,
+  },
 })
