e11979196eb6338a163376e6b11b0e7d944f7010
徐康 <7836246@qq.com>
2024-09-16 21:12:44 +0800

    :zap: 增加项目

--- server/api/project.ts
+++ server/api/project.ts
@@ -1,7 +1,13 @@
 export default defineEventHandler(() => [
   {
     name: "Nuxt-Whois",
-    description: "基于Nuxt3制作的Whois和 Dns 查询的小工具。",
+    description: "基于 Nuxt3 制作的 Whois 和 Dns 查询的小工具。",
     link: "https://github.com/7836246/Nuxt-Whois",
+  },
+  {
+    name: "KangGo",
+    description:
+      "用 Go 语言编写的轻量级 Web 框架，旨在提供简单易用的开发体验。",
+    link: "https://github.com/7836246/kanggo",
   },
 ]);
